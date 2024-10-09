e8db2d3e35de47c0cb6974150f6cdb295df56c9a
Sai Manoj Gaddipati <30746079+sai-manoj-gaddipati@users.noreply.github.com>
2024-10-09 00:45:24 -0700

    Update solutions.java

--- solutions.java
+++ solutions.java
@@ -1,3 +1,7 @@
+// Problem#2: Sum root to leaf nodes
+// Time complexity: O(N)
+// Space Complexity: O(H) where H is height of tree (when we use recursion each step goes into a stack and gets popped when done
+// Algorithm: Recursively call child nodes until leaf node is reached, when leaf node is reached add the sum to global variable 'result', repeat for all leaf nodes
 class Solution {
     int result;
     public int sumNumbers(TreeNode root) {
@@ -11,11 +15,9 @@ class Solution {
         curSum = curSum*10 + node.val;
         if (node.left == null && node.right == null){
             result += curSum;
-            return;
         }
 
         helper(node.left, curSum);
         helper(node.right, curSum);
-    return;
     }
 }
